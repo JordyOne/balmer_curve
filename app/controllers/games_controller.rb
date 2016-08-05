4c7eca572cc57eb9e6611402df74070643b0163b
JordyOne <jordan.t.dale@gmail.com>
2016-08-05 15:47:20 -0600

    more stuff

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
   def show
+    def show
+      @game = Game.find_by(player_1_id: params[:id])
+      @game ||= Game.find_by(player_2_id: params[:id])
 
+      Player_1  
+    end
   end
 end
