d246eaf27ff63a3e4630e410017a218cc42cbfe9
JordyOne <jordan.t.dale@gmail.com>
2016-08-05 16:44:38 -0600

    final routing changes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,7 @@
 class GamesController < ApplicationController
- def create
-   @game = Game.find_by(player_1_id: session[:player_id])
-   @game ||= Game.find_by(player_2_id: session[:player_id])
-
-
-   redirect_to games_show_path(@game)
- end
   def show
-    @game = Game.find(params[:id])
+    @game = Game.find_by(player_1_id: session[:player_id])
+    @game ||= Game.find_by(player_2_id: session[:player_id])
 
     @player_1 = Player.find(@game.player_1_id)
     @player_2 = Player.find(@game.player_2_id)
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,15 +9,21 @@ class PlayersController < ApplicationController
   end
 
   def create
-    player = Player.new(player_params)
     available_players = Player.where(has_game: false)
+    player = Player.new(player_params)
     if available_players.empty?
       player.has_game = false
+      opponent = nil
+    else
+      player.has_game = true
+      opponent = available_players.first
     end
     if player.save
       session[:player_id] = player.id
-      opponent = available_players.first
-      Game.create(player_1_id: player.id, player_2_id: opponent.id)
+      unless opponent.nil?
+        @game = Game.new(player_1_id: player.id, player_2_id: opponent.id)
+        opponent.update_attribute(:has_game, true) if @game.save
+      end
       redirect_to players_path
     else
       render json: {errors: player.errors.full_errors}, status: :bad_request
