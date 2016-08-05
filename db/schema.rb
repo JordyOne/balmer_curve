a4df45248d5d90af54c9876fc6fb4f94b3f2a1e8
JordyOne <jordan.t.dale@gmail.com>
2016-08-05 14:52:39 -0600

    player migration fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805203305) do
-
-  create_table "players_tables", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
