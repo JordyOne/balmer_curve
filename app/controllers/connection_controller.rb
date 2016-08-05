7cdb73c2548577d8bbcac1c3b02d41819f7ca9a4
dlinch <dlinch33@gmail.com>
2016-08-05 15:38:11 -0600

    migration

--- app/controllers/connection_controller.rb
+++ app/controllers/connection_controller.rb
@@ -5,12 +5,12 @@ class ConnectionController < WebsocketRails::BaseController
    end
 
    # The :client_connected method is fired automatically when a new client connects
- def :client_connected
+ def client_connected
  end
 # The :client_disconnected method is fired automatically when a client disconnects
  def client_disconnected
  end
 # The :connection_closed method is fired automatically when a client loses connection without sending a disconnect frame.
- def :connection_closed
+ def connection_closed
  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805203305) do
 ActiveRecord::Schema.define(version: 20160805212534) do
 
   create_table "games", force: :cascade do |t|
